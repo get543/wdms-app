b781144c4079bc6cee18420133ce461160d4e72c
get543 <fghaza.pratama@gmail.com>
2026-07-12 10:50:35 +0700

    fix: Update webUrl to point to Vercel deployment

--- wdms-mobile/lib/constants/app_constants.dart
+++ wdms-mobile/lib/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   AppConstants._();
 
   static const String appTitle = "WDMS Mobile";
-  static const String webUrl = "https://pc.goldeye-temperature.ts.net";
-  // static const String webUrl = "http://192.168.18.4:5173";
+  static const String webUrl = "https://wdms-app.vercel.app";
+  // static const String webUrl = "https://pc.goldeye-temperature.ts.net";
   // static const String webUrl = "http://10.0.2.2:5173"; # works with emulator
 }
